5f2503a9b3828fd6b115f80ffb0e34dd3288f250
Kirill <traineratwot@yandex.ru>
2023-09-25 23:20:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SimpleCache2",
-    version="1.0.0",
+    version="1.0.1",
     author="Traineratwot",
     author_email="Traineratwot@yandex.ru",
     description="Cache system for python",
